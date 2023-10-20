81a26ce3ac78f6b588e4cb5546b763698a8a524f
Gigi-U <andanlibres@gmail.com>
2023-10-20 15:52:11 -0300

    fixing exercise class 4

--- digitalhouse/module1/week2/c4ComPuntInterf/pgc4/exercise.go
+++ digitalhouse/module1/week2/c4ComPuntInterf/pgc4/exercise.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 )
 
-/* Objetivo
-El objetivo de esta guía práctica es que podamos afianzar los conceptos sobre estructuras e interfaces. Para esto vamos a plantear un ejercicio simple e incremental, que nos permitirá repasar los temas que estudiamos.
+/* 
 Consigna
+
 Una empresa de redes sociales requiere implementar una estructura usuarios con funciones que vayan agregando información a la misma.
-Para optimizar y ahorrar memoria requieren que la estructura usuarios ocupe el mismo lugar en memoria para el main del programa y para las funciones.
 
-La estructura debe tener los campos: nombre, apellido, edad, correo y contraseña. Y deben implementarse las funciones:
+Para optimizar y ahorrar memoria requieren que la estructura usuarios ocupe el mismo lugar en memoria para el main del programa y para las funciones. (Punteros)
 
-cambiarNombre: permite cambiar el nombre y apellido.
-cambiarEdad: permite cambiar la edad.
-cambiarCorreo: permite cambiar el correo.
-cambiarContraseña: permite cambiar la contraseña. 
-
-! NO ENTIENDO SI TENGO QUE HACER UNA INTERFAZ , O SOLO CAMBIAR LOS DATOS DE LA ESTRUCTURA CON PUNTERO.
+	- La estructura debe tener los campos: 
+		nombre
+		apellido
+		edad
+		correo 
+		contraseña
+	- Y deben implementarse las funciones:
+		cambiarNombre: permite cambiar el nombre y apellido.
+		cambiarEdad: permite cambiar la edad.
+		cambiarCorreo: permite cambiar el correo.
+		cambiarContraseña: permite cambiar la contraseña. 
 
 
 */
@@ -32,65 +36,80 @@ type Usuario struct{
 }
 
 
+func (u Usuario) InformacionUsuario() {
+	fmt.Printf(
+		" Usuario: %s %s %d %s %s \n", 
+		u.Nombre, 
+		u.Apellido, 
+		u.Edad, 
+		u.Correo, 
+		u.Contrasena,
+	)
+}
+
+
+func cambiarNombre(u *Usuario) string{
+	u.Nombre = "Cosme"
+	return u.Nombre
+}
+ func cambiarApellido( u *Usuario)string{
+	u.Apellido = "Fulanito"
+	return u.Apellido
+}
+func cambiarEdad(u *Usuario) int{
+	u.Edad = 70
+	return u.Edad
+}
+func cambiarCorreo(u *Usuario) string{
+	u.Correo = "cf@gmail.com"
+	return u.Correo
+}
+func cambiarContrasena(u *Usuario) string{
+	u.Contrasena = "1111111"
+	return u.Contrasena 
+} 
+
 // or main()  
 func InfoUsuario(){
-
-	u1 := Usuario{
-		Nombre:   "Gisela",
-		Apellido: "Urriza",
-		Edad: 43,
-		Correo: "gu@gmail.com",
+	// cargando datos en el Usuario inicial y seteando el puntero
+	u := &Usuario{
+		Nombre:   	"Gisela",
+		Apellido: 	"Urriza",
+		Edad: 		43,
+		Correo: 	"gu@gmail.com",
 		Contrasena: "123456",
 	}
+	// imprimiendo valor inicial de Usuario
+	fmt.Sprintln( "el valor inicial de Usuario es: ")
+	u.InformacionUsuario()
 
-	u2:=&u1
-	// Impresión usuario1
-	u1.InformacionUsuario()
-	
-	// Impresión usuario2
-	*u2 = newUser("Cosme","Fulanito",70,"cf@gmail.com","654321")
-	u2.InformacionUsuario()
+	//	LLamado a funciones de cambiod e datos
+	cambiarNombre(u)
+	cambiarApellido(u)
+	cambiarEdad(u)
+	cambiarCorreo(u)
+	cambiarContrasena(u)
+	// imprimiendo valor final de usuario
+	fmt.Sprintln( "el valor final de Usuario es: ")
+	u.InformacionUsuario()
 
+	// Estructura de control para comprobar que el puntero apunta a la variable
+	// 1) Verifica si el puntero se creó correctamente
+	if u != nil {
+		fmt.Println("El puntero a Usuario se creó correctamente.")
 
+		// 2) Imprime la dirección de memoria del puntero
+		fmt.Printf("Dirección del puntero: %p\n", u)
+
+		// 3) Imprimela dirección de memoria de la variable apuntada
+		fmt.Printf("Dirección de la variable apuntada: %p\n", &u.Nombre)
+	} else {
+		fmt.Println("El puntero a Usuario no se creó correctamente.")
+	}
 }
-
-func (u Usuario) InformacionUsuario() {
-	fmt.Printf(" Usuario: %s %s %d %s %s , Dirección: %v \n", u.Nombre, u.Apellido, u.Edad, u.Correo, u.Contrasena, &u.Nombre)
-
-}
-
-type user interface {
-	cambiarNombre() string
-	cambiarApellido() string
-	cambiarEdad() int
-	cambiarCorreo() string
-	cambiarContrasena()  string
-}
-func newUser(nombre string, apellido string, edad int , correo string, contraseña string) Usuario {
-	return Usuario{Nombre: nombre, Apellido: apellido, Edad: edad, Correo: correo, Contrasena: contraseña}
-}
-
-func (u Usuario) cambiarNombre( nombre string){
-	u.Nombre = nombre
-}
-func (u Usuario) cambiarApellido( apellido string){
-	u.Apellido = apellido
-}
-func (u Usuario)cambiarEdad(edad int){
-	u.Edad = edad
-}
-func (u Usuario) cambiarCorreo(correo string){
-	u.Correo = correo
-}
-func (u Usuario) cambiarContrasena(contrasena string){
-	u.Contrasena = contrasena
-}
-
-
-
 
 
 // sin tengo 2 metodos con el mismo nombre y con la misma firma, correrlo.
 //en el caso de las funciones no se puede
 
-//!return fmt.Sprintf("hola %s", p.Nombre)
+//return fmt.Sprintf("hola %s", p.Nombre)
